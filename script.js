129dc829dc47ef5c02213058732c8c929bde5a1e
Mitzi <mitzi.arg28@gmail.com>
2024-12-13 15:46:25 -0500

    latest

--- script.js
+++ script.js
@@ -40,16 +40,15 @@ class PerishableProductProperties extends ProductProperties {
     }
 }
 
-let product1 = new ProductProperties('Lipbalm', 2.50, 4);
-let product2 = new ProductProperties('Makeup', 20.20, 6);
-let perishableProduct1 = new PerishableProductProperties('Milk', 1.50, 10, '2025-9-03');
-let perishableProduct2 = new PerishableProductProperties('Yogurt', 2.00, 15, '2025-06-28');
-
 // Part 4
 class Store {
     constructor() {
         this.inventory = []; 
     } 
+
+    addProduct(product) {
+        this.inventory.push(product); // Add product to inventory
+    }
 
     getInventoryValue() {
         return this.inventory.reduce((total, product) => total + product.getTotalValue(), 0);
@@ -60,18 +59,37 @@ class Store {
     }
 }
 
+let product1 = new ProductProperties('Lipbalm', 2.50, 4);
+let product2 = new ProductProperties('Makeup', 20.20, 6);
+let product3 = new ProductProperties('Hairties', 5.00, 8);
+let perishableProduct1 = new PerishableProductProperties('Milk', 1.50, 10, '2025-9-03');
+let perishableProduct2 = new PerishableProductProperties('Yogurt', 2.00, 15, '2025-06-28');
+
+
 //Part 3 Apply a 10% discount 
-ProductProperties.applyDiscount([product1, product2, perishableProduct1, perishableProduct2], 0.1);
+ProductProperties.applyDiscount([product1, product2, product3, perishableProduct1, perishableProduct2], 0.1);
 
 let store = new Store();
+store.addProduct(product1);
+store.addProduct(product2);
+store.addProduct(product3);
+store.addProduct(perishableProduct1);
+store.addProduct(perishableProduct2);
+
 console.log(product1.toString());
 console.log(product2.toString());
+console.log(product3.toString()); 
 console.log(perishableProduct1.toString()); 
 console.log(perishableProduct2.toString());
 
 console.log("Total Inventory Value (Before Discount): $", store.getInventoryValue().toFixed(2));
 
+ProductProperties.applyDiscount(store.inventory, 0.1);
+
+console.log("Total Inventory Value (After 10% Discount): $", store.getInventoryValue().toFixed(2));
+
 ProductProperties.applyDiscount(store.inventory, 0.15);
+
 console.log("Total Inventory Value (After 15% Discount): $", store.getInventoryValue().toFixed(2));
 
 
