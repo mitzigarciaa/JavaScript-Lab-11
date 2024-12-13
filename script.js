7b52569f16a6f4fb96c3443deff2f5ce111ce9b5
Mitzi <mitzi.arg28@gmail.com>
2024-12-13 15:01:47 -0500

    part 1

---
+++ script.js
@@ -0,0 +1,29 @@
+// Base class for product properties
+class ProductProperties {
+    constructor(name, price, quantity) {
+        this.name = name;
+        this.price = price;
+        this.quantity = quantity;
+    }
+
+
+    // Method
+    getTotalValue() {
+        return this.price * this.quantity;
+    }
+
+
+    // Method to return a string representation of the product
+    toString() {
+        return `Product: ${this.name}, Price: $${this.price.toFixed(2)}, Quantity: ${this.quantity}`;
+    }
+}
+
+
+// Example
+let product = new ProductProperties('Apple', 2.50, 50);
+console.log(product.getTotalValue());
+console.log(product.toString());
+
+
+
